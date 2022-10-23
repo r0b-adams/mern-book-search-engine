7fa24b5a02a991ba48137fd3fc7857e3b10a789b
Robert Adams <adamsiii.robert@gmail.com>
2022-10-23 16:44:05 -0700

    Refactor Express app with Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,40 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { join } from "path";
+import { typeDefs, resolvers } from "./schemas";
+import { connection } from "./config";
+import { ENV } from "./utils/constants";
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // serve static assets in produciton
+  if (process.env.NODE_ENV === ENV.PRODUCTION) {
+    app.use(express.static(join(__dirname, "../client/build")));
+  }
+
+  // serve React client
+  app.get("/", (_req, res) => {
+    res.sendFile(join(__dirname, "../client/build/index.html"));
+  });
+
+  await server.start(); // start Apollo server
+  server.applyMiddleware({ app }); // attach to Express server
+  await connection(); // connect to MongoDB
+
+  app.listen(PORT, () => {
+    console.log(`App listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
+
+startApolloServer();
