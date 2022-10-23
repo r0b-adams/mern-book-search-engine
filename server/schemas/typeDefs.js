2e864efd126239c774b27d2a782089dba73c2177
Robert Adams <adamsiii.robert@gmail.com>
2022-10-23 16:51:53 -0700

    Add basic Query types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,14 +27,9 @@ export const typeDefs = gql(`
   }
 
   # QUERIES
-  type Query {}
+  type Query {
+    users: [User]
+    books: [Book]
+  }
 
-  # MUTATIONS
-  type Mutation {}
 `);
-
-// The ID scalar type represents a unique identifier,
-// often used to refetch an object or as the key for a cache.
-// The ID type is serialized in the same way as a String;
-// however, defining it as an ID signifies that it is
-// not intended to be human‐readable.
