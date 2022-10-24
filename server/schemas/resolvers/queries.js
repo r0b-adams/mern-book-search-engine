8c80a17a9da360cbc2183b75f56b430649ae6c57
Robert Adams <adamsiii.robert@gmail.com>
2022-10-23 17:43:45 -0700

    Move queries to own module

---
+++ server/schemas/resolvers/queries.js
@@ -0,0 +1,12 @@
+import { AuthenticationError } from "apollo-server-express";
+import { User } from "../../models";
+
+export const queries = {
+  users: () => User.find(),
+  user: async (_parent, _args, context) => {
+    if (context.user) {
+      return User.findById(context.user._id);
+    }
+    throw new AuthenticationError("Please log in");
+  },
+};
