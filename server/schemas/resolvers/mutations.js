5afeaaf6e2ce46c210217c8290bfdef4bde6a661
Robert Adams <adamsiii.robert@gmail.com>
2022-10-23 17:43:34 -0700

    Move mutations to own module

---
+++ server/schemas/resolvers/mutations.js
@@ -0,0 +1,6 @@
+export const mutations = {
+  signup: async (parent, args) => {},
+  login: async (parent, args) => {},
+  // saveBook: async (parent, args, context) => {},
+  // deleteBook: async (parent, args, context) => {},
+};
