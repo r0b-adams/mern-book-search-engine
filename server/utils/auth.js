337f4ea304a41f418459c3cdb40ea1676e145758
Robert Adams <adamsiii.robert@gmail.com>
2022-11-08 12:15:55 -0800

    simplify jwt payload and add context notes

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,23 +1,40 @@
 import jwt from "jsonwebtoken";
+import { AuthenticationError } from "apollo-server-express";
 
+// create a jwt with user ID as payload
+export const signToken = user => {
+  return jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRATION,
+  });
+};
+
+// req instance of Express Incoming Message
 export const context = ({ req }) => {
+  // grab the auth header
   const auth = req.headers.authorization;
+
+  // make sure it exists
   if (auth) {
+    // ["Bearer", "<token>"]
     const token = auth.split(" ").pop().trim();
+
+    // if the token exists, validate and decode data
+    // then attach to req object
     if (token) {
-      const { user } = jwt.verify(token, process.env.JWT_SECRET, {
-        maxAge: process.env.JWT_EXPIRATION,
-      });
-      req.user = user;
+      try {
+        const { userID } = jwt.verify(token, process.env.JWT_SECRET, {
+          maxAge: process.env.JWT_EXPIRATION,
+        });
+        req.userID = userID;
+
+        // malformed token, invalid token, expired token
+      } catch (error) {
+        console.log(error.message);
+        throw new AuthenticationError(error.message);
+      }
     }
   }
   return req;
-};
-
-export const signToken = ({ _id, username, email }) => {
-  return jwt.sign({ _id, username, email }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRATION,
-  });
 };
 
 // // set token secret and expiration date
