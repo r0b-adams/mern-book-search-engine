68aa2f894f7767d9a4f7aa9b65c80a5d4d27ecd6
Robert Adams <adamsiii.robert@gmail.com>
2022-11-08 21:08:00 -0800

    add removeBook mutation

--- client/src/gql/mutations.js
+++ client/src/gql/mutations.js
@@ -58,4 +58,21 @@ export const SAVE_BOOK = gql(`
   }
 `);
 
-// export const REMOVE_BOOK = gql(``);
+export const REMOVE_BOOK = gql(`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`);
