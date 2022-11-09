5cd81d1ea579c74df8b94327d14a09abaed4543c
Robert Adams <adamsiii.robert@gmail.com>
2022-11-08 16:13:22 -0800

    add addUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,29 @@
 import { gql } from "@apollo/client";
 
-export const ADD_USER = gql(``);
+export const ADD_USER = gql`
+  mutation addUser($payload: AddUserInput!) {
+    addUser(payload: $payload) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
 
-export const LOGIN_USER = gql(``);
+// export const LOGIN_USER = gql(``);
 
-export const SAVE_BOOK = gql(``);
+// export const SAVE_BOOK = gql(``);
 
-export const REMOVE_BOOK = gql(``);
+// export const REMOVE_BOOK = gql(``);
